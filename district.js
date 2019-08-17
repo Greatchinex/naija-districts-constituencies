f8feae33c9154469fc1f43b7cc8cea6cf3b02fae
Chilaka Chinedu <greatchinex@gmail.com>
2019-08-17 12:07:01 +0100

    Data Structure for the Districts

---
+++ district.js
@@ -0,0 +1,154 @@
+const disitricts = [
+  {
+    state: "Abia",
+    districts: ["Abia North", "Abia South", "Abia Central"]
+  },
+  {
+    state: "Adamawa",
+    districts: ["Adamawa North", "Adamawa South", "Adamawa Central"]
+  },
+  {
+    state: "Akwa-ibom",
+    districts: [
+      "Akwa-ibom North-East",
+      "Akwa-ibom North-West",
+      "Akwa-ibom South"
+    ]
+  },
+  {
+    state: "Anambra",
+    districts: ["Anambra North", "Anambra Central", "Anambra South"]
+  },
+  {
+    state: "Bauchi",
+    districts: ["Bauchi North", "Bauchi Central", "Bauchi South"]
+  },
+  {
+    state: "Bayelsa",
+    districts: ["Bayelsa East", "Bayelsa Central", "Bayelsa West"]
+  },
+  {
+    state: "Benue",
+    districts: ["Benue North-East", "Benue North-West", "Benue South"]
+  },
+  {
+    state: "Borno",
+    districts: ["Borno North", "Borno Central", "Borno South"]
+  },
+  {
+    state: "Cross-River",
+    districts: ["Cross-River North", "Cross-River Central", "Cross-River South"]
+  },
+  {
+    state: "Delta",
+    districts: ["Delta North", "Delta Central", "Delta South"]
+  },
+  {
+    state: "Ebonyi",
+    districts: ["Ebonyi North", "Ebonyi Central", "Ebonyi South"]
+  },
+  {
+    state: "Edo",
+    districts: ["Edo North", "Edo Central", "Edo South"]
+  },
+  {
+    state: "Ekiti",
+    districts: ["Ekiti North", "Ekiti Central", "Ekiti South"]
+  },
+  {
+    state: "Enugu",
+    districts: ["Enugu North", "Enugu East", "Enugu West"]
+  },
+  {
+    state: "Gombe",
+    districts: ["Gombe North", "Gombe Central", "Gombe South"]
+  },
+  {
+    state: "Imo",
+    districts: ["Imo North", "Imo East", "Imo West"]
+  },
+  {
+    state: "Jigawa",
+    districts: ["Jigawa North-West", "Jigawa North-East", "Jigawa South-West"]
+  },
+  {
+    state: "Kaduna",
+    districts: ["Kaduna North", "Kaduna Central", "Kaduna South"]
+  },
+  {
+    state: "Kano",
+    districts: ["Kano North", "Kano Central", "Kano South"]
+  },
+  {
+    state: "Katsina",
+    districts: ["Katsina North", "Katsina Central", "Katsina South"]
+  },
+  {
+    state: "Kebbi",
+    districts: ["Kebbi North", "Kebbi Central", "Kebbi South"]
+  },
+  {
+    state: "Kogi",
+    districts: ["Kogi West", "Kogi Central", "Kogi East"]
+  },
+  {
+    state: "Kwara",
+    districts: ["Kwara North", "Kwara Central", "Kwara South"]
+  },
+  {
+    state: "Lagos",
+    districts: ["Lagos West", "Lagos Central", "Lagos East"]
+  },
+  {
+    state: "Nassarawa",
+    districts: ["Nassarawa North", "Nassarawa West", "Nassarawa South"]
+  },
+  {
+    state: "Niger",
+    districts: ["Niger North", "Niger East", "Niger South"]
+  },
+  {
+    state: "Ogun",
+    districts: ["Ogun Central", "Ogun East", "Ogun West"]
+  },
+  {
+    state: "Ondo",
+    districts: ["Ondo North", "Ondo Central", "Ondo South"]
+  },
+  {
+    state: "Osun",
+    districts: ["Osun Central", "Osun East", "Osun West"]
+  },
+  {
+    state: "Oyo",
+    districts: ["Oyo North", "Oyo Central", "Oyo South"]
+  },
+  {
+    state: "Plateau",
+    districts: ["Plateau North", "Plateau Central", "Plateau South"]
+  },
+  {
+    state: "Rivers",
+    districts: ["Rivers East", "Rivers West", "Rivers South"]
+  },
+  {
+    state: "Sokoto",
+    districts: ["Sokoto North", "Sokoto East", "Sokoto South"]
+  },
+  {
+    state: "Taraba",
+    districts: ["Taraba North", "Taraba Central", "Taraba South"]
+  },
+  {
+    state: "Yobe",
+    districts: ["Yobe North", "Yobe East", "Yobe South"]
+  },
+  {
+    state: "Zamfara",
+    districts: ["Zamfara North", "Zamfara Central", "Zamfara West"]
+  },
+  {
+    state: "FCT",
+    districts: ["FCT"]
+  }
+];
